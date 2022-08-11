42a593b06526e7fd46562fa310674db38d1f01c8
“pitlimitCode” <prabow14@gmail.com>
2022-08-11 15:53:32 +0700

    deployTry

--- model/db.js
+++ model/db.js
@@ -17,7 +17,7 @@ if (process.env.ENV_MODE === "prod") {
 		database: process.env.DB_NAME,
 		password: process.env.DB_PASS,
 		port: process.env.DB_PORT,
-		sslmode: require
+		sslmode: true
 	});
 }
 module.exports = conn;
