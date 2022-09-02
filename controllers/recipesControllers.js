507ab4e2f822b11e41239b40a3531b259c305b89
pitlimitCode <prabow14@gmail.com>
2022-09-02 21:07:13 +0700

    [edit] total like for all recipe & detail recipe

--- controllers/recipesControllers.js
+++ controllers/recipesControllers.js
@@ -33,6 +33,37 @@ const showAll = async (req, res) => {
   }
 };
 
+// FIND RECIPE BY ID
+const showById = async (req, res) => {
+  try {
+    // const { id } = req.query;
+    const id = parseInt(req.params.id);
+    if(isNaN(id)){ return res.json({ StatusCode: 400, isValid: false, message: `Id data-type must integer`, }); }
+    const show = await model.showById(id);
+    if(show.rowCount == 0){ return res.json({ StatusCode: 200, isValid: true, message: `No one Recipe id: '${id}' on Database.`, }); }
+    
+    return res.json({ StatusCode: 200, isValid: true, data: show.rows, });
+    
+  } catch (err) {
+    console.log(err);
+    return res.json({ StatusCode: 500, isValid: false, message: err.message, });
+  }
+};
+
+// FIND RECIPES BY NAME
+const showByName = async (req, res) => {
+  try {
+    const name = req.params.name;
+    const nameLower = name.toLowerCase();
+    const show = await model.showByName(nameLower);
+    if(show.rowCount == 0){ return res.json({ StatusCode: 200, isValid: true, message: `No one Recipe include words: '${name}' from recipes data`, }); }
+    return res.json({ StatusCode: 200, isValid: true, result: { count_of_data: show.rowCount, data: show.rows }, });
+  } catch (err) {
+    console.log(err);
+    return res.json({ StatusCode: 500, isValid: false, message: err.message, });
+  }
+};
+
 // SHOW RECIPES IN PAGES
 const showInPages = async (req, res) => {
   try {
@@ -77,37 +108,6 @@ const showNew = async (req, res) => {
   try {
     const show = await model.showNew();
     if(show.rowCount == 0){ return res.json({ StatusCode: 200, isValid: true, message: `No one recipe on Database`, }); }
-    return res.json({ StatusCode: 200, isValid: true, result: { count_of_data: show.rowCount, data: show.rows }, });
-  } catch (err) {
-    console.log(err);
-    return res.json({ StatusCode: 500, isValid: false, message: err.message, });
-  }
-};
-
-// FIND RECIPE BY ID
-const showById = async (req, res) => {
-  try {
-    // const { id } = req.query;
-    const id = parseInt(req.params.id);
-    if(isNaN(id)){ return res.json({ StatusCode: 400, isValid: false, message: `Id data-type must integer`, }); }
-    const show = await model.showById(id);
-    if(show.rowCount == 0){ return res.json({ StatusCode: 200, isValid: true, message: `No one Recipe id: '${id}' on Database.`, }); }
-    
-    return res.json({ StatusCode: 200, isValid: true, data: show.rows, });
-    
-  } catch (err) {
-    console.log(err);
-    return res.json({ StatusCode: 500, isValid: false, message: err.message, });
-  }
-};
-
-// FIND RECIPES BY NAME
-const showByName = async (req, res) => {
-  try {
-    const name = req.params.name;
-    const nameLower = name.toLowerCase();
-    const show = await model.showByName(nameLower);
-    if(show.rowCount == 0){ return res.json({ StatusCode: 200, isValid: true, message: `No one Recipe include words: '${name}' from recipes data`, }); }
     return res.json({ StatusCode: 200, isValid: true, result: { count_of_data: show.rowCount, data: show.rows }, });
   } catch (err) {
     console.log(err);
@@ -263,10 +263,10 @@ const deleteRecipe = async (req, res) => {
 
 module.exports = {
   showAll,
-  showInPages,
-  showNew,
   showById,
   showByName,
+  showInPages,
+  showNew,
   newRecipe,
   newVideo,
   editImage,
--- routes/recipesRoutes.js
+++ routes/recipesRoutes.js
@@ -5,10 +5,10 @@ const multiUpload = require("../middleware/multiUploadVideo");
 const singleUploadRecipe = require("../middleware/singleUploadRecipe");
 
 Router.get("/recipes/all/", controller.showAll); // SHOW ALL RECIPES
-Router.get("/recipes/pagination/", controller.showInPages); // SHOW RECIPES IN PAGES
-Router.get("/recipes/fivenew", controller.showNew); // SHOW 5 NEW RECIPES
 Router.get("/recipes/id/:id", controller.showById); // FIND RECIPE BY ID
 Router.get("/recipes/name/:name", controller.showByName); // FIND RECIPE BY NAME
+Router.get("/recipes/pagination/", controller.showInPages); // SHOW RECIPES IN PAGES
+Router.get("/recipes/fivenew", controller.showNew); // SHOW 5 NEW RECIPES
 Router.post("/recipes/add", verifyToken.checkToken, singleUploadRecipe.single('image'), controller.newRecipe); // ADD NEW RECIPE
 Router.post("/recipes/addvideo", verifyToken.checkToken, multiUpload.array('video', 4), controller.newVideo); // ADD VIDEO TO RECIPE
 Router.patch("/recipes/editimage", verifyToken.checkToken, singleUploadRecipe.single('image'), controller.editImage); // EDIT RECIPE DATA BY ID
