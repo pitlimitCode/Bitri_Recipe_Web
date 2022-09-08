e5ac0b0260a97014440fa4bf90d3f8abdb048a98
pitlimitCode <prabow14@gmail.com>
2022-09-08 12:44:10 +0700

    [fix error] try catch

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -6,18 +6,20 @@ const checkToken = async (req, res, next) => {
   try{
     // console.log(req);
     // console.log(req.headers['authorization']);
-    jwt.verify(req.headers['authorization'].split(' ')[1], process.env.JWT_KEY, function(err, decoded) {
-      if (err) {
-        // console.log(err);
-        if(err.message == "jwt expired") { return res.json({ StatusCode: 400, isValid: false, message: err.message }) }
-        return res.status(400).send('Error verify type: ' + err.message + '.');
-      } else {
-        // console.log(decoded);
-        // console.log(decoded.id)
-        req.tokenUserId = decoded.id;
-        next();
-      }
-    })
+    if(req.headers['authorization']){
+      jwt.verify(req.headers['authorization'].split(' ')[1], process.env.JWT_KEY, function(err, decoded) {
+        if (err) {
+          // console.log(err);
+          if(err.message == "jwt expired") { return res.json({ StatusCode: 400, isValid: false, message: err.message }) }
+          return res.status(400).send('Error verify type: ' + err.message + '.');
+        } else {
+          // console.log(decoded);
+          // console.log(decoded.id)
+          req.tokenUserId = decoded.id;
+          next();
+        }
+      })
+    }
     
   } catch (err) {
     console.log(err);
